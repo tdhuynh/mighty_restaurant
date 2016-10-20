fba81ab6fd573ca23c98326aa4657802edbc1003
tdhuynh <tommyhuynh93@gmail.com>
2016-10-20 16:14:38 -0400

    made user create view

--- mighty_restaurant/mighty_restaurant/urls.py
+++ mighty_restaurant/mighty_restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from restaurant_app.views import UserCreateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
+    url(r'^$', UserCreateView.as_view(), name='user_create_view'),
 ]
