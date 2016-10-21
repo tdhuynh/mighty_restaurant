48e314d695392bdde962a37ac61354c29060ffdf
destanton <delisestanton@gmail.com>
2016-10-20 22:46:46 -0400

    added links in base html, and formatted create user and login page

--- mighty_restaurant/mighty_restaurant/urls.py
+++ mighty_restaurant/mighty_restaurant/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from restaurant_app.views import UserCreateView
+from restaurant_app.views import UserCreateView, OrderCreateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
+    url(r'^$', OrderCreateView.as_view(), name='order_create_view'),
     url(r'^create_user/$', UserCreateView.as_view(), name='user_create_view'),
 ]
--- mighty_restaurant/restaurant_app/views.py
+++ mighty_restaurant/restaurant_app/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from restaurant_app.models import Order
 
 
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = "/"
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    success_url = "/"
+    fields = ('item', 'table', 'notes')
