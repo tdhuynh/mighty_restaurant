a9f02fc8de4831fdccc54a5205b60dce223e3986
destanton <delisestanton@gmail.com>
2016-10-20 22:17:58 -0400

    added create user page, minor formatting to base html

--- mighty_restaurant/mighty_restaurant/urls.py
+++ mighty_restaurant/mighty_restaurant/urls.py
@@ -6,5 +6,5 @@ from restaurant_app.views import UserCreateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
-    url(r'^$', UserCreateView.as_view(), name='user_create_view'),
+    url(r'^create_user/$', UserCreateView.as_view(), name='user_create_view'),
 ]
--- mighty_restaurant/restaurant_app/views.py
+++ mighty_restaurant/restaurant_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
