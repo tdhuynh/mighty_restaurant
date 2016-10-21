4a1130de1d2f2b510cc23c1551f2191cede1be26
tdhuynh <tommyhuynh93@gmail.com>
2016-10-21 10:36:12 -0400

    home view and template

--- mighty_restaurant/mighty_restaurant/urls.py
+++ mighty_restaurant/mighty_restaurant/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from restaurant_app.views import UserCreateView, OrderCreateView
+from restaurant_app.views import UserCreateView, OrderCreateView, HomeView, ProfileUpdateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
-    url(r'^$', OrderCreateView.as_view(), name='order_create_view'),
+    url(r'^$', HomeView.as_view(), name='home_list_view'),
+    url(r'^order/$', OrderCreateView.as_view(), name='order_create_view'),
     url(r'^create_user/$', UserCreateView.as_view(), name='user_create_view'),
+    url(r'^accounts/profile/$', ProfileUpdateView.as_view(), name='profile_update_view'),
 ]
--- mighty_restaurant/restaurant_app/views.py
+++ mighty_restaurant/restaurant_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DetailView, ListView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from restaurant_app.models import Order
+from restaurant_app.models import Order, Profile, Table, Item
+
+
+class HomeView(ListView):
+    template_name = "home.html"
+    model = Table
 
 
 class UserCreateView(CreateView):
@@ -20,3 +26,11 @@ class OrderCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context["order"] = Order.objects.all()
         return context
+
+class ProfileUpdateView(UpdateView):
+    template_name = 'profile.html'
+    fields = ('access_level',)
+    success_url = '/'
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
